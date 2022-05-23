34e048be3432b2ef0821bd1bd0996b0bcfd9cc98
Oebele Lijzenga <o.r.lijzenga@student.utwente.nl>
2022-05-23 13:04:50 +0200

    Implement Game, test suite needs to be expanded.

--- src/main/java/com/patchwork/app/backend/GameResult.java
+++ src/main/java/com/patchwork/app/backend/GameResult.java
@@ -5,8 +5,10 @@ import java.util.Map;
 public class GameResult {
 
     public Map<Player, Integer> playerScores;
+    public Player winner;
 
-    public GameResult(Map<Player, Integer> playerScores) {
+    public GameResult(Map<Player, Integer> playerScores, Player winner) {
         this.playerScores = playerScores;
+        this.winner = winner;
     }
 }
