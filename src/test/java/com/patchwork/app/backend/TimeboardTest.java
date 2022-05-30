bab4ac6979a47d0675ebdf5dc0d3f152cbf1b0a7
Kannepally Koushik <koushik.koushik.kumar@gmail.com>
2022-05-31 01:04:59 +0200

    Added Test Cases for Timeboard

---
+++ src/test/java/com/patchwork/app/backend/TimeboardTest.java
@@ -0,0 +1,71 @@
+package com.patchwork.app.backend;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeboardTest {
+
+    @Test
+    public void constructTimeboardTest() {
+        // Assert a Timeboard with  52 time spaces got created when called constructor
+
+        List<Player> players = new ArrayList<>();;
+        players.add(new Player("Player 1"));
+        players.add(new Player("Player 2"));
+
+        TimeBoard timeBoard = new TimeBoard(players);
+        Assert.assertEquals(52, timeBoard.spaces.size());
+    }
+
+    @Test
+    public void testInitialPositionsOfPlayers(){
+        List<Player> players = new ArrayList<>();;
+        players.add(new Player("Player 1"));
+        players.add(new Player("Player 2"));
+        TimeBoard timeBoard = new TimeBoard(players);
+
+        // Assert if the initial position of players in Timerboard is in the first space
+        Assert.assertEquals(0, timeBoard.getPlayerPosition(players.get(0)));
+        Assert.assertEquals(0, timeBoard.getPlayerPosition(players.get(1)));
+    }
+
+    @Test
+    public void checkPositionOfPlayerAfterMoved(){
+        List<Player> players = new ArrayList<>();;
+        players.add(new Player("Player 1"));
+        players.add(new Player("Player 2"));
+        TimeBoard timeBoard = new TimeBoard(players);
+
+        // Assert if the player has moved to the correct position of space after it is moved
+        timeBoard.movePlayer(players.get(1), 5);
+        Assert.assertEquals(5,timeBoard.getPlayerPosition(players.get(1)));
+
+        // Since Player-1 is not moved check if he is in 0th space and not 5th space as Player-2
+        Assert.assertFalse( timeBoard.getPlayerPosition(players.get(0)) == 5 );
+        Assert.assertEquals(0,timeBoard.getPlayerPosition(players.get(0)));
+    }
+
+    @Test
+    public void testGetCurrentPlayer(){
+        List<Player> players = new ArrayList<>();;
+        players.add(new Player("Player 1"));
+        players.add(new Player("Player 2"));
+        TimeBoard timeBoard = new TimeBoard(players);
+        timeBoard.movePlayer(players.get(1), 5);
+
+        // Now Since Player-2 is ahead in Timer board Assert if the current player is Player-1
+        Assert.assertEquals(players.get(0), timeBoard.getCurrentPlayer());
+
+        // Move the Player-1 ahead of Player-2 and check if the current player is Player-2 since he is behind in Timer board
+        timeBoard.movePlayer(players.get(0), 10);
+        Assert.assertEquals(players.get(1), timeBoard.getCurrentPlayer());
+
+        // Move the Player-2 to the same position of Player-1 making it sit over the top and Assert if the current player is Player-2
+        timeBoard.movePlayer(players.get(1), 10);
+        Assert.assertEquals(players.get(1), timeBoard.getCurrentPlayer());
+        Assert.assertFalse(players.get(0) == timeBoard.getCurrentPlayer());
+    }
+}
